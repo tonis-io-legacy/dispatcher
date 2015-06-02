5794e2757854109fdea87797a171efd476cb5a55
Kyle Spraggs <theman@spiffyjr.me>
2015-06-02 11:16:01 -0500

    Test cleanup

--- test/bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-include __DIR__.'/bootstrap.const.php';
-
-require APPLICATION_ROOT . '/vendor/autoload.php';
-
-require APPLICATION_ROOT . '/test/TestAsset/Invokable.php';
-require APPLICATION_ROOT . '/test/TestAsset/TestDispatchable.php';
